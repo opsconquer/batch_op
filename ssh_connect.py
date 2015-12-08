4f2f682a2c8a0e99de1c6eeab3a992e387fd6612
yyglider <yaoyuan0120@126.com>
2015-12-08 11:23:16 +0800

    修改批量下载文件功能，重命名下载文件
    重命名下载文件，文件名中增加主机名的标识

--- ssh_connect.py
+++ ssh_connect.py
@@ -6,6 +6,9 @@ import paramiko
 import os
 import string
 
+import confRead
+
+
 import iniLog
 
 infoLogger,errorLogger=iniLog.ini()
@@ -68,7 +71,7 @@ def filesUpload(hostname,port,username,password,local_dir,remote_dir):
         errorLogger.error(hostname+" failed!-------------------- ")
 
 
-		
+
 #文件下载
 def filesDownload(hostname,port,username,password,local_dir,remote_dir):
     try:
@@ -83,40 +86,33 @@ def filesDownload(hostname,port,username,password,local_dir,remote_dir):
             sftp.get(remote_path,local_path)
             print "download %s from remote %s" % (files,remote_dir)
             infoLogger.info("download %s from remote %s successfully" % (local_path,remote_path))
+            oldfile=os.path.join(os.getcwd(),local_dir[:-1]+os.sep+str(files))
+            newfile=os.path.join(os.getcwd(),local_dir[:-1]+os.sep+str(hostname)+"_"+str(files))
+            os.rename(oldfile,newfile)
+        print '%s is processed over--------------------'%(hostname)
+        infoLogger.info(hostname+" is processed over---------------")
         t.close()
     except Exception,e:
+        print e
         print '%s failed!-------------------- '%(hostname)
         errorLogger.error(hostname+" failed!-------------------- ")
-        print e
 
 
-		
+        
 if __name__=='__main__':
-    cmd=[]
-    #循环读取脚本文件
-    script_list=confRead.read_configBySection('config.ini', 'SCRIPTS')
-    print script_list
-    for each_script in script_list:                     
-        #从脚本中读取执行的命令列表
-        scriptFile=open(each_script[1]) #打开脚本文件
-        line=scriptFile.readlines() #用readlines读取脚本文件到列表
-        for m in line:
-            cmd.append(m.strip())
-
-    #从配置文件中读取连接参数：HOST_CONF = host:port:username:passwd
-    #循环执行
     host_list = confRead.read_configBySection('config.ini', 'HOST')
+    remote_dir = confRead.read_config('config.ini', 'DOWNLOADS_REMOTE_DIR', 'REMOTE_DIR')
     for each_host in host_list:
         hostConf_list = each_host[1].strip().split(':')
-        ip = hostConf_list[0]
+        hostname = hostConf_list[0]
         port = hostConf_list[1]
         username = hostConf_list[2]
         passwd = hostConf_list[3]
-        ssh_cmd(ip,port,username,passwd,cmd)
-        #多线程执行
-        #threads = []   
-        #a=threading.Thread(target=ssh,args=(ip,port,username,passwd,cmd))
-        #a.start()
-	
-	
-	
+        local_dir = 'downloads/'    
+        #filesUpload(hostname,port,username,passwd,local_dir,remote_dir)
+        filesDownload(hostname,port,username,passwd,local_dir,remote_dir)
+
+
+    
+    
+    
